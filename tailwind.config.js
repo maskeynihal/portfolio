418c03ec4bf2f876ca0573c44716ada2aa49a5f3
maskeynihal <maskeynihal@gmail.com>
2021-03-27 15:27:48 +0545

    Add configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'media',
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
